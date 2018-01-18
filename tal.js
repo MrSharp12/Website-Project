d7641784666e5413d5c6a42ef5fdbe3d910230eb
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-01-17 23:17:16 -0500

    added comments

--- tal.js
+++ tal.js
@@ -1,22 +1,24 @@
 'use strict'
 
-const NAV = document.querySelector('#mainNav');
-const TOP_NAV_BAR = NAV.offsetTop; //grabs top of the navbar on page load
+const NAV = document.querySelector('#mainNav');//gets the nav element
+const TOP_NAV_BAR = NAV.offsetTop; //gets the distance between top of screen and top of nav element
 let scroll = new SmoothScroll('a[href*="#"]'); //links to smooth-scroll.js
 let cartTotal = 0;
 
-const submitInfo = () => document.getElementById('cartCount').value = ++cartTotal;
+const submitInfo = () => document.getElementById('cartCount').value = ++cartTotal;//increases cart total by one, prints to DOM
 const stickyNav = ()  => {
-    if(window.scrollY >= TOP_NAV_BAR) {
+    if(window.scrollY >= TOP_NAV_BAR) {//once window scrolls down to top of nav bar...
+        //eliminates scroll jump by adding padding to the top
+        //padding equals viewable height of the NAV const value in pixels
         document.body.style.paddingTop = NAV.offsetHeight + 'px';
-        document.body.classList.add('sticky-nav');
+        document.body.classList.add('sticky-nav');//adds sticky nav class to nav & logo element
     } else {
-        document.body.style.paddingTop = 0; //this takes care of the extra space at top, prevents site from jumping up
+        document.body.style.paddingTop = 0;
         document.body.classList.remove('sticky-nav');
     }
 }
 
-window.addEventListener('scroll', stickyNav);
+window.addEventListener('scroll', stickyNav);//activates the sticky nav process on scroll
 
 
 
